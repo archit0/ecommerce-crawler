ce568f0f6ecd4189ed63006da7b943ecad1b22fe
Archit Dwivedi <architdwivedi00@gmail.com>
2018-06-17 21:45:21 +0530

    Flipkart product list crawling added

---
+++ flipkart/product_list.py
@@ -0,0 +1,35 @@
+import requests
+import logging
+import warnings
+from bs4 import BeautifulSoup
+
+# Loggers and warning
+warnings.filterwarnings("ignore", category=UserWarning, module='bs4')
+
+# constants
+ITEM_CONTAINER = 's-item-container'
+PRODUCTS_CLASS_NAME = 's-access-detail-page'
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+products_url = "https://www.flipkart.com/search?q=wildcraft&marketplace=FLIPKART&otracker=start&as-show=on&as=off&page={page}"
+
+f = open("products.csv", "w+")
+
+parsed = {}
+page = 1
+while True:
+
+    print('Product Page: {}'.format(page))
+    product_page_url = products_url.format(page=page)
+    page += 1
+    source = requests.get(product_page_url, headers=HEADERS).content
+    soup = BeautifulSoup(source)
+    rows = soup.findAll('div', class_='_3O0U0u')
+    if len(rows) == 0:
+        break
+    for each_row in rows:
+        for each_product in each_row.children:
+            ele = each_product.findAll('a')[1]
+            href = 'https://www.flipkart.com/{}'.format(ele['href'])
+            title = ele.text
+            f.write("{href}|@|{title}\n".format(title=title, href=href))
