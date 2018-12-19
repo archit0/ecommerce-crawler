8d7a7cc002ea7719bd418f0b4a5e14cb5990a77d
archit0 <architdwivedi00@gmail.com>
2018-12-19 12:25:10 +0530

    Added

--- crawler/crawl.py
+++ crawler/crawl.py
@@ -2,10 +2,52 @@ from crawler.amazon import amazon
 
 keyword = 'fiber supplements'
 
-product_data = amazon.get_data(keyword=keyword, domain='co.uk', max_products=2)
+config = {
+    'co.uk': {
+        'fiber supplements': {},
+        'Citrucel': {},
+        'Garden of Life Raw Fiber': {},
+        'Optimum Nutrition': {},
+        'Metamucil': {},
+        'Yerba': {},
+        'Renew Life': {},
+        'Now Fiber': {},
+    },
+    'in': {
+        'Benefiber': {},
+        'Citrucel': {},
+        'Garden of Life Raw Fiber': {},
+        'Optimum Nutrition': {},
+        'Metamucil': {},
+        'Yerba': {},
+        'Renew Life': {},
+        'Now Fiber': {},
+        'Organic India Fiber': {},
+    },
+    'com': {
+        'Benefiber': {},
+        'Citrucel': {},
+        'Garden of Life Raw Fiber': {},
+        'Optimum Nutrition': {},
+        'Metamucil': {},
+        'Yerba': {},
+        'Renew Life': {},
+        'Now Fiber': {},
+    }
+}
+responses = {}
+for each_domain, domain_keywords in config.items():
+    for each_keyword in domain_keywords:
+        domain_keywords[each_keyword] = amazon.get_data(
+            keyword=keyword,
+            domain=each_domain,
+            max_page=1
+        )
+
 
 file_name = "{}.json".format(keyword.replace(' ', '_').lower().strip())
 
 import json
+
 f = open(file_name, "w+")
-f.write(json.dumps(product_data))
+f.write(json.dumps(responses))
